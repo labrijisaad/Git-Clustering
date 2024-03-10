81185b89d2a364450b996dc44013a51d77fccc5f
labri <labrijisaad@gmail.com>
2024-03-11 00:36:27 +0100

    added tests for different dataset before packaging

--- src/dataloaders/real_dataloader.py
+++ src/dataloaders/real_dataloader.py
@@ -19,16 +19,16 @@ class Real_DataLoader:
             df = pd.read_csv(osp.join(self.path, self.name + ".csv"), header=None)
 
         if self.name == "iris":
-            X = df.iloc[1:, :-1].values.astype(np.float)
-            Y_true = df.iloc[1:, -1].values.astype(np.float)
+            X = df.iloc[1:, :-1].values.astype(float)
+            Y_true = df.iloc[1:, -1].values.astype(float)
         elif self.name == "wine":
-            X = df.iloc[1:, :-1].values.astype(np.float)
-            Y_true = df.iloc[1:, -1].values.astype(np.int)
+            X = df.iloc[1:, :-1].values.astype(float)
+            Y_true = df.iloc[1:, -1].values.astype(int)
             Y_set = list(set(Y_true))
             Y_map = {Y_set[i]: i for i in range(len(Y_set))}
             Y_true = np.array([Y_map[y] for y in Y_true])
         elif self.name == "breast_cancer":
-            X = df.iloc[:, 2:].values.astype(np.float)
+            X = df.iloc[:, 2:].values.astype(float)
             Y_true = df.iloc[:, 1]
             Y_set = list(set(Y_true))
             Y_map = {Y_set[i]: i for i in range(len(Y_set))}
@@ -37,8 +37,8 @@ class Real_DataLoader:
             df.replace("?", np.nan, inplace=True)
             df = df.apply(pd.to_numeric, errors="coerce")
             df = df.fillna(df.mean())
-            X = df.iloc[1:, 1:].values.astype(np.float)
-            Y_true = df.iloc[1:, 0].values.astype(np.int)
+            X = df.iloc[1:, 1:].values.astype(float)
+            Y_true = df.iloc[1:, 0].values.astype(int)
             Y_set = list(set(Y_true))
             Y_map = {Y_set[i]: i for i in range(len(Y_set))}
             Y_true = np.array([Y_map[y] for y in Y_true])
